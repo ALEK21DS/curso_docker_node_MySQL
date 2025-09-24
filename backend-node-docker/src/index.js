f5f3080cb88724641e0c823ee31f38777fe9a08f
ALEK21DS <brandonalexpesantez@gmail.com>
2025-09-23 19:39:40 -0500

    Variables de desarrollo

--- backend-node-docker/src/index.js
+++ backend-node-docker/src/index.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const port = process.env.PORT;
+const jwtSecret = process.env.JWT_SECRET;
 
 app.use(express.json());
+console.log("clave secreta: ", jwtSecret);
+console.log("puerto: ", port);
 
 app.get('/', (req, res) => {
     res.send('API backend funcionando');
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>{
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
+app.listen(port, ()=>{
+    console.log(`Servidor escuchando en el puerto ${port}`);
 });
